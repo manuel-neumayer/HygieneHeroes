df6fc2086a0736c1f46cf523923fbd7efc90c047
quang-nguyenln <dnguyen2172@gmail.com>
2022-09-28 18:24:13 -0700

    finish spitting guy

--- The Dungeon Game/Dungeon 4 IT-Talents/guns.js
+++ The Dungeon Game/Dungeon 4 IT-Talents/guns.js
@@ -58,7 +58,7 @@ function Gun1(owner, pathweight) {
     rotate(-this.rotation)
     translate(-(x + (this.owner.x * w)), -(y + (this.owner.y * w)))
     for (gui = 0; gui < this.protectiles.length; gui++) {
-      this.protectiles[gui].disp(x, y, w)
+      this.protectiles[gui].disp(x, y, w )
     }
   }
 
@@ -602,7 +602,7 @@ function Mouth(owner, pathweight) {
   this.owner = owner
   this.cost = 0
   this.costs = 0
-  this.shootingspeed = 60
+  this.shootingspeed = 200
   this.minuslives = 1
   this.col = [255, 0, 0, 255]
   this.strokecol = [51, 51, 51, 255]
@@ -629,11 +629,11 @@ function Mouth(owner, pathweight) {
 
   this.shootSeveral = function() {
     this.owner.cash -= this.costs
-    for (angle = -PI/16; angle <= PI/16; angle += PI/32) {
+    for (angle = 0; angle <= PI/8; angle += PI/32) {
       var vec1 = createVector(this.owner.pointingAt[0] - this.x, this.owner.pointingAt[1] - this.y)
       vec1.setMag(this.protectilespeed)
       vec1.rotate(angle)
-      var newprotectile = new Protectile(this, this.owner.chamber, this.owner, this.x, this.y, vec1.x, vec1.y, this.owner.chamber.pathweight)
+      var newprotectile = new Spit(this, this.owner.chamber, this.owner, this.x, this.y, vec1.x, vec1.y, this.owner.chamber.pathweight)
       this.protectiles.push(newprotectile)
     }
   }
@@ -663,9 +663,7 @@ function Mouth(owner, pathweight) {
     translate(-(x + (this.owner.x * w)), -(y + (this.owner.y * w)))
     fill(this.col)
     stroke(this.strokecol)
-    //rectMode(CENTER)
-    //rect(x + ((this.owner.x * w)), y + (this.owner.y - (this.owner.height/2))*w, this.width * w, this.height* w)
-    //rectMode(CORNER)
+    
     ellipse(x + ((this.owner.x * w)), y + (this.owner.y - (this.owner.height/4))*w, this.height/2 * w, this.width* w)
     fill(this.col)
     translate(x + (this.owner.x * w), y + (this.owner.y * w))
@@ -699,5 +697,57 @@ function Mouth(owner, pathweight) {
     player.guns.push(newgun)
     player.gun = newgun
     player.cash -= this.cost
+  }
+}
+
+function Spit(gun, chamber, owner, x, y, speedX, speedY, pathweight) {
+  this.gun = gun
+  this.chamber = chamber
+  this.owner = owner
+  this.x = this.owner.x
+  this.y = this.owner.y
+  this.width = this.gun.size
+  this.height = this.gun.size * (6 / 8)
+  this.speedX = speedX
+  this.speedY = speedY
+  var vec1 = createVector(speedX, speedY)
+  this.rotation = map(degreeVector(vec1), 360, 0, 0, TWO_PI)
+  this.col = [30,144,255, 255]
+
+  this.update = function(enemy) {
+    this.x += this.speedX 
+    this.y += this.speedY
+    for (pui = 0; pui < this.chamber.monsters.length; pui++) {
+      if (this.owner.index) {
+        if (enemy.within(this.x, this.y)) {
+          this.gun.action(enemy)
+          return true
+        }
+      } else {
+        if (this.chamber.monsters[pui].within(this.x, this.y)) {
+          this.gun.action(this.chamber.monsters[pui])
+          return true
+        }
+      }
+    }
+    if (this.x < this.chamber.x || this.y < this.chamber.y || this.x > this.chamber.x + this.chamber.width || this.y > this.chamber.y + this.chamber.height) {
+      return true
+    }
+  }
+
+
+  this.disp = function(x, y, w) {
+    translate(x + (this.x * w), y + (this.y * w))
+    rotate(this.rotation)
+    translate(-(x + (this.x * w)), -(y + (this.y * w)))
+    fill(this.col)
+    noStroke()
+    //rectMode(CENTER)
+    //rect(x + ((this.owner.x * w)), y + (this.owner.y - (this.owner.height/2))*w, this.width * w, this.height* w)
+    //rectMode(CORNER)
+    rect(x + (this.x  * w), y + (this.y * w), this.width * w, this.height * w, (this.height / 4) * w)
+    translate(x + (this.x * w), y + (this.y * w))
+    rotate(-this.rotation)
+    translate(-(x + (this.x * w)), -(y + (this.y * w)))
   }
 }
