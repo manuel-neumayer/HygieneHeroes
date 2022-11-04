f2413fa22c4b8da5bc0a1e0bf4a40ed6320c1c66
matt-rem <mattrem@berkeley.edu>
2022-11-04 04:23:46 -0700

    Added chamber orientation

--- The Dungeon Game/Dungeon 4 IT-Talents/dungeon.js
+++ The Dungeon Game/Dungeon 4 IT-Talents/dungeon.js
@@ -4,7 +4,6 @@
 function Dungeon() {
   this.level = 1
   this.player = new Player()
-  this.gamemode = new GameMode(this.player)
   //Die .w Variable gibt Größe der Darstellung der Map in Pixeln an. Also die Seitenlänge der quadratischen Map. (Siehe .disp())
   this.w = height
   //Die .x und .y Variablen geben den linken, oberen Eckpunkt der Map an und damit, wie weit sie verschoben ist (Siehe .update() und .disp()).
@@ -241,12 +240,10 @@ function Dungeon() {
       if (random(1) < 0.65) {
         //this.chambers[i].addBottle()
         this.chambers[i].addHuman(ceil(random(5)))
-        this.chambers[i].addCArea()
       } else {
         //this.chambers[i].addSink()
           //this.chambers[i].addDesk()
       }
-      
       if (this.level >= 5 && random(1) < (1 / 30)) {
         if (this.level >= 12) {
           //Manche Monster tauchen erst in späteren Leveln auf.
@@ -342,7 +339,6 @@ function Dungeon() {
       this.setup(10)
     }
     //Damit ist die .update() function bereits beendet!
-    this.gamemode.update()
   }
 
   //Die .disp() function visualisiert die Map, alle ihre Kammern und Monster und den Spieler. Sie kümmert sie auch um das Sichtfeld
@@ -664,7 +660,7 @@ function BuyKeys() {
     fill(0)
     textSize(boxheight / 10)
     text("Get " + this.keys + " temporary", x + 10, y)
-    y += textSize()
+    y += textSize() + 5
     text("keys", x + 10, y)
   }
 
